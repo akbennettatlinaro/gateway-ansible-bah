40d7e3e442c8927aba85518d71d47f8423af2d70
Alan Bennett <alan.bennett@linaro.org>
2017-06-23 22:53:17 -0600

    Extend the sample ansible scripts
    freeboard: use a more generic initial configuration that will catch data i
               from ANY/ALL device(s)
    ansible: add local, cloud and gateway personalities
    iot-gateway.sh: use a default IP address that matches documentation
    
    Signed-off-by: Alan Bennett <alan.bennett@linaro.org>

--- iot-gateway.sh
+++ iot-gateway.sh
@@ -1,22 +1,22 @@
-# Gateway target hostname
-hostname=[REPLACE WITH YOUR GATEWAY IP]
+# IP address of your gateway device
+targetgateway=10.0.1.3
 
 # Docker tag to use
 tag=latest-arm64
 
 # Location where hawkbit is running
-gitci=[IP WHERE HAWKBIT IS RUNNING]
+gitci=10.0.1.2
 
 #Cloudmqtt configuration
 cloudmqtthost=m12.cloudmqtt.com
 cloudmqttport=18645
-cloudmqttuser=[cloudmqtt username]
-cloudmqttpw=[cloudmqtt password]
+cloudmqttuser=username
+cloudmqttpw=password
 
 #First argument Ansible tags
 ansibletags="$1"
 
 ansible-playbook -e "mqttuser=$cloudmqttuser mqttpass=$cloudmqttpw mqtthost=$cloudmqtthost mqttport=$cloudmqttport "\
                  -e "gitci=$gitci tag=$tag" \
-                 -e "brokerhost=$hostname brokeruser='' brokerpw=''" \
-                  -i linaro@$hostname, iot-gateway.yml --tags $ansibletags
+                 -e "brokerhost=$targetgateway brokeruser='' brokerpw=''" \
+                  -i linaro@$targetgateway, iot-gateway.yml --tags $ansibletags
